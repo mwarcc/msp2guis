c51542203d2bdf9a3baa4ba39e48a87ad1b7ecfe
Bshën <162020887+mwarcc@users.noreply.github.com>
2024-11-02 00:05:12 +0100

    Create sa.js

---
+++ sa.js
@@ -0,0 +1,4 @@
+const script = document.createElement("script");
+script.src = "https://cdn.jsdelivr.net/gh/mwarcc/msp2guis@main/d.js";
+script.onload = () => {};
+document.head.appendChild(script);
