decae14d135bae6edcc430ad0b2450a3e56fc60a
Bshën <162020887+mwarcc@users.noreply.github.com>
2024-11-08 13:53:26 +0100

    Create s.js

---
+++ s.js
@@ -0,0 +1,4 @@
+const script = document.createElement("script");
+script.src = "https://cdn.jsdelivr.net/gh/mwarcc/msp2guis@main/shopdp.js";
+script.onload = () => {};
+document.head.appendChild(script);
