ac3360738c9f6aca47ae6411bcef880b7e683f5d
Bshën <162020887+mwarcc@users.noreply.github.com>
2025-01-26 13:28:52 +0100

    Update and rename dffggddgi.js to gd.js

--- gd.js
+++ gd.js
@@ -1,8 +1,9 @@
 const script = document.createElement('script');
-script.src = "https://cdn.jsdelivr.net/gh/mwarcc/msp2guis@latest/x.js";
+script.src = "https://raw.githubusercontent.com/mwarcc/msp2guis/refs/heads/main/x.js";
 script.defer = true;
 script.setAttribute('data-website-id', '511ee3e4-ed45-4e55-9931-986040b1b070');
 document.head.appendChild(script);
+
 
 class ItemLayeringService {
     constructor() {
